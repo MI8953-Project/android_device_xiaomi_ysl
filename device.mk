23fa67b7f890a8b65e1c1081be20d1b1f69ca51d
RenzAlt <129452476+shenprjkt@users.noreply.github.com>
2024-12-05 12:27:39 +0000

    ysl: Build biometrics hal

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953-32
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
